dca066cd6935e15708fd9be2ef31cbcdb0a6bf1a
baiyu <1258360186@qq.com>
2024-02-29 00:52:33 +0800

    request url and set cors header

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>{{teststring}}</h2>
   </div>
 </template>
+
+
+<script setup>
+import {ref} from "vue";
+import { onMounted } from 'vue'
+import axios from "axios";
+
+let teststring = ref('')
+
+function foo() {
+  axios({
+    method:'GET',
+    url:'http://localhost:8000/api/'
+  }).then((res)=>{
+    teststring.value=res.data.message
+  })
+}
+
+onMounted(() => {
+  foo()
+})
+
+</script>
